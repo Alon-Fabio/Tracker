c670911ad4e44dbaff98dd87785a8c52906dbeaf
Alon-Fabio <alon.the.fabio@gmail.com>
2021-05-15 19:12:09 +0300

    First commit!

--- App.tsx
+++ App.tsx
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import AccountScreen from "./src/screens/AccountScreen";
+import SigninScreen from "./src/screens/SigninScreen";
+import SingupScreen from "./src/screens/SingupScreen";
+import TrackCreateScreen from "./src/screens/TrackCreateScreen";
+import TrackDetailScreen from "./src/screens/TrackDetailScreen";
+import TrackListScreen from "./src/screens/TrackListScreen";
+
+interface TrackListFlow {
+  TrackDetail: undefined;
+  TrackList: undefined;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+interface ISwitchNav {
+  logFlow: { Signin: undefined; Signup: undefined };
+  mainFlow: {
+    TrackListFlow: TrackListFlow;
+    TrackCreate: undefined;
+    Account: undefined;
+  };
+}
+
+const switchNavigator = createSwitchNavigator({
+  logFlow: createStackNavigator({ Signin: SigninScreen, Signup: SingupScreen }),
+  mainFlow: createBottomTabNavigator({
+    TrackListFlow: createStackNavigator({
+      TrackDetail: TrackDetailScreen,
+      TrackList: TrackListScreen,
+    }),
+    TrackCreate: TrackCreateScreen,
+    Account: AccountScreen,
+  }),
 });
+
+export default createAppContainer(switchNavigator);
